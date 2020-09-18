bdeff1d487ea4348bb448027c3235d1a472d9d80
Suphada Thongthai <614259054@webmail.npru.ac.th>
2020-09-19 00:44:50 +0700

    Add Profile

---
+++ data.js
@@ -0,0 +1,38 @@
+const data ={
+    "name" : "Miss. Suphada Thongthai",
+    "work" : "Student at Software Enginering , Faculty of Science and Technology Nakhon Pathom Rajabhat University",
+    "aboutme":{
+        "FullName" :"Suphada thongthai",
+        "Phone" : "(+66)6 3628 1264",
+        "Email" : "suphada22.ae@gmail.com",
+        "Address" : "85 Moo 7 Takut Rai, ChonDaen, Phetchabun 67190"
+    },
+    "education" :[
+        {
+            "SchoolName" : "St.Neo College of Technology",
+            "Gradelevel" : "Professional certificate",
+            "EndEducation" : "2017",
+            "Major" : "Business Computer",
+            "GPA": "3.56"    
+        },
+        {
+            "UniversityName" : "Nakhon Pathom Rajabhat University",
+            "Grade level" : "Bachelor's degree",
+            "Faculty" : "Science and Technology",
+            "Major" : "Software Engineering",
+            "GPA" : "3.00"
+        }
+    ],
+    "workexperience" : [
+        {
+            "WorkName" : "Silathawee Chok Co., Ltd..",
+            "WorkingTime" : "March 2016 - July 2016",
+            "Position" : "Accounting clerk",
+            "JobDescription" : "- Increase understanding of the office work process Silathawee Chok Company Limited by helping managers with various documents."
+            
+        },
+      
+    ],
+    "personalSkills" :["Java" , "SQL" , "PHP" , "C" , "C++" , "Node JS" , "HTML"]
+};
+module.exports = data;
